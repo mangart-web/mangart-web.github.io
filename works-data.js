8616b72707bfab996c151bd5dd6861d08d3c5067
mangart-web <mangart@gmx.ch>
2026-07-27 19:05:25 +0200

    Update works-data.js

--- works-data.js
+++ works-data.js
@@ -1,1 +1,365 @@
+window.mangartWorks = [
+  {
+    id: "work-01",
 
+    title: {
+      de: "Werk 01",
+      en: "Work 01"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 01",
+          en: "Front view of Work 01"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 01",
+          en: "Detail view of Work 01"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 01 in einer Raumansicht",
+          en: "Work 01 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-02",
+
+    title: {
+      de: "Werk 02",
+      en: "Work 02"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 02",
+          en: "Front view of Work 02"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 02",
+          en: "Detail view of Work 02"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 02 in einer Raumansicht",
+          en: "Work 02 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-03",
+
+    title: {
+      de: "Werk 03",
+      en: "Work 03"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 03",
+          en: "Front view of Work 03"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 03",
+          en: "Detail view of Work 03"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 03 in einer Raumansicht",
+          en: "Work 03 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-04",
+
+    title: {
+      de: "Werk 04",
+      en: "Work 04"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 04",
+          en: "Front view of Work 04"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 04",
+          en: "Detail view of Work 04"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 04 in einer Raumansicht",
+          en: "Work 04 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-05",
+
+    title: {
+      de: "Werk 05",
+      en: "Work 05"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 05",
+          en: "Front view of Work 05"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 05",
+          en: "Detail view of Work 05"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 05 in einer Raumansicht",
+          en: "Work 05 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-06",
+
+    title: {
+      de: "Werk 06",
+      en: "Work 06"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 06",
+          en: "Front view of Work 06"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 06",
+          en: "Detail view of Work 06"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 06 in einer Raumansicht",
+          en: "Work 06 shown in an interior setting"
+        }
+      }
+    ]
+  },
+
+  {
+    id: "work-07",
+
+    title: {
+      de: "Werk 07",
+      en: "Work 07"
+    },
+
+    year: "2026",
+
+    medium: {
+      de: "Acryl auf Leinwand",
+      en: "Acrylic on canvas"
+    },
+
+    size: "100 × 120 cm",
+
+    text: {
+      de: "Der kuratorische Werktext folgt.",
+      en: "The curatorial text will follow."
+    },
+
+    cover: "",
+
+    images: [
+      {
+        type: "front",
+        src: "",
+        alt: {
+          de: "Frontansicht von Werk 07",
+          en: "Front view of Work 07"
+        }
+      },
+      {
+        type: "detail",
+        src: "",
+        alt: {
+          de: "Detailansicht von Werk 07",
+          en: "Detail view of Work 07"
+        }
+      },
+      {
+        type: "installation",
+        src: "",
+        alt: {
+          de: "Werk 07 in einer Raumansicht",
+          en: "Work 07 shown in an interior setting"
+        }
+      }
+    ]
+  }
+];
